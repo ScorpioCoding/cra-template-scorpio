dea73a30cc2a4187608956276fdc09567cbecd59
denOldTimer <kribooutlook.com>
2024-06-27 10:37:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React template scorpio (cra-template-scorpio)
+
+### cra-template-scorpio
+
+Is a react boilerpalte template
+
+### React Command Line
+
+`npx create-react-app <some-name> --template scorpio`
---
+++ template/src/App.js
@@ -0,0 +1,18 @@
+import logo from './assets/img/scorpiocoding-banner.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-slogan">
+          <h3>{process.env.REACT_APP_SITE_NAME}</h3>
+          <code>npx create-react-app `some-app-name` --template scorpio</code>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
